3e37a6be6e918a78bd29303ebe8df2e5b506b970
Mustafa Turkoz <mturkoz93@gmail.com>
2023-06-29 00:53:23 +0300

    added build settings

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,19 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    lib: {
+      entry: 'src/components/index.js',
+      name: 'MyCounter',
+      formats: ['es', 'umd'],
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+  },
 })
